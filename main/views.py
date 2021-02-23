f5693e4350abc513134e543e9482f9eabd41499b
zafaris <zafaris.dev@gmail.com>
2021-02-23 01:07:03 +0000

    Fixed bug if no records for that user

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ import sys
 
 from django.contrib.auth.decorators import login_required
 from django.db.models import Avg, Count, Min, Sum
+from django.db.models.functions import Coalesce
 from django.shortcuts import redirect, render
 from django.utils import dateparse, timezone
 
@@ -21,7 +22,7 @@ def leaderboard(request):
     for user_leaderboard in UserLeaderboard.objects.all():
         user = user_leaderboard.user
         user_records = FitnessRecord.objects.order_by('-created').filter(user=user, created__month=timezone.now().month)
-        agg_stats = user_records.aggregate(total_calories=Sum('calories'), total_duration=Sum('duration'))
+        agg_stats = user_records.aggregate(total_calories=Coalesce(Sum('calories'), 0), total_duration=Coalesce(Sum('duration'), 0))
         agg_stats['user'] = user
         agg_stats['last_record'] = user_records[0].created
 
