b6261d0294311a6da2b1252e45476cfc7e42246c
zafaris <zafaris.dev@gmail.com>
2021-02-23 01:17:35 +0000

    Fixed duration default

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ def leaderboard(request):
     for user_leaderboard in UserLeaderboard.objects.all():
         user = user_leaderboard.user
         user_records = FitnessRecord.objects.order_by('-created').filter(user=user, created__month=timezone.now().month)
-        agg_stats = user_records.aggregate(total_calories=Coalesce(Sum('calories'), 0), total_duration=Coalesce(Sum('duration'), 0))
+        agg_stats = user_records.aggregate(total_calories=Coalesce(Sum('calories'), 0), total_duration=Coalesce(Sum('duration'), datetime.timedelta()))
         agg_stats['user'] = user
         agg_stats['last_record'] = user_records[0].created
 
