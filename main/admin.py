c27c0d53f149b6bf3b66867b8157c2af7c109ff6
zafaris <zafaris.dev@gmail.com>
2021-02-25 22:25:04 +0000

    Added Family model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from main.models import FitnessRecord
+from main.models import FitnessRecord, Family
 
 # Register your models here.
 admin.site.register(FitnessRecord)
+admin.site.register(Family)
--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.utils.crypto import get_random_string
+
 
 # Create your models here.
 class FitnessRecord(models.Model):
@@ -13,11 +15,25 @@ class FitnessRecord(models.Model):
         WALK = 'Walk'
         RUN = 'Run'
         SPORTS = 'Sports'
-     
+
     category = models.CharField(max_length=10, choices=CategoryChoices.choices, default=CategoryChoices.BIKE)
 
-    calories = models.IntegerField(default=5)
-    duration = models.DurationField(default='00:05:00', help_text='HH:MM:ss format')
+    calories = models.IntegerField(default=100)
+    duration = models.DurationField(default='00:30:00', help_text='HH:MM:ss format')
 
     def __str__(self):
-        return f'{self.user.username}, {self.category}, {self.calories}'
+        return f'{self.user.username} | {self.category} | {self.calories}'
+
+def generateCode():
+    return get_random_string(length=6)
+
+class Family(models.Model):
+    name = models.CharField(max_length=20)
+    members = models.ManyToManyField(User)
+    code = models.CharField(max_length=6, default=generateCode, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Families'
+    
+    def __str__(self):
+        return f'{self.name} | {self.members.all().count()} members | {self.code}'
